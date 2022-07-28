b2f1a61ca44105940efeb930559d4d757c5b622e
biai <110030580+bitree-hub@users.noreply.github.com>
2022-07-28 12:53:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # New section to allow the user pick a fruit
-
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
+
+#
+my_cur.execute ("insert into fruit_load_list values('from streamlit')
